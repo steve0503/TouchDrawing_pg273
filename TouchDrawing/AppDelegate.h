37b7a06010120dcd3a3a1fcd53e92c87aeeab491
steve0503 <leehr76@gmail.com>
2014-01-09 14:43:44 +0900

    Initial Commit

---
+++ TouchDrawing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TouchDrawing
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchDrawingTests/TouchDrawingTests.m
@@ -0,0 +1,34 @@
+//
+//  TouchDrawingTests.m
+//  TouchDrawingTests
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TouchDrawingTests : XCTestCase
+
+@end
+
+@implementation TouchDrawingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
